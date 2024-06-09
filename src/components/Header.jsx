45f7e48baa93b42ea6a89bdb67a5191385b5d509
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-06-09 17:39:39 +0100

    Complete Header componenet for desktop version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,21 @@ const Header = () => {
   return (
     <header className="w-full h-screen relative bg-cover bg-center bg-[url('./desktop/image-header.jpg')]">
       <Nav />
+      <div className="flex flex-col items-center pt-44">
+        <h1 className="text-6xl uppercase font-fraunces font-extrabold tracking-[12px] text-neutral-white">We are creatives</h1>
+        <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2">
+          <g
+            stroke="#FFF"
+            stroke-width="6"
+            fill="none"
+            fill-rule="evenodd"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M18 3v100M3 95.484l15 15 15-15" />
+          </g>
+        </svg>
+      </div>
     </header>
   );
 };
--- src/components/src/Nav.jsx
+++ src/components/src/Nav.jsx
@@ -1,26 +1,33 @@
 const Nav = () => {
   return (
-    <div className="container">
-      <div className="nav flex justify-between items-center py-8">
-        <a href="" className="logo">
-          <img src="./logo.svg" alt="Sunnyside" />
-        </a>
-        <ul className="nav-list flex gap-11">
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Services</a>
-          </li>
-          <li>
-            <a href="">Projects</a>
-          </li>
-          <li className="">
-            <a href="" className="font-fraunces py-4 px-8 bg-neutral-white rounded-full border-primary-light-blue border-1 uppercase">Contact</a>
-          </li>
-        </ul>
+    <nav className="absolute top-0 left-0 right-0">
+      <div className="container">
+        <div className="nav flex justify-between items-center py-8">
+          <a href="" className="logo">
+            <img src="./logo.svg" alt="Sunnyside" />
+          </a>
+          <ul className="nav-list flex gap-11">
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Services</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li className="">
+              <a
+                href=""
+                className="font-fraunces py-4 px-8 bg-neutral-white rounded-full border-primary-light-blue border-1 uppercase"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
