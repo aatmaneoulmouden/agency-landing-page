fc3d581833e410c40ab24d33ac20c21cf489bc13
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-06-21 19:06:34 +0100

    Update images for mobile

--- src/components/src/SingleProject.jsx
+++ src/components/src/SingleProject.jsx
@@ -1,7 +1,8 @@
 const SingleProject = (props) => {
   return (
     <div className="project">
-      <img src={`./desktop/${props.image}.jpg`} alt="Project" />
+      <img src={`./desktop/${props.image}.jpg`} alt="Project" className="hidden lg:block" />
+      <img src={`./mobile/${props.image}.jpg`} alt="Project" className="block lg:hidden" />
     </div>
   );
 };
