a92478e59e1a5367b536cf18145011698ba09020
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-06-22 22:00:40 +0100

    Services - update images for mobile version

--- src/components/src/SingleService.jsx
+++ src/components/src/SingleService.jsx
@@ -8,7 +8,12 @@ const SingleService = (props) => {
       <img
         src={`./desktop/${image}.jpg`}
         alt={title}
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover hidden lg:block"
+      />
+      <img
+        src={`./mobile/${image}.jpg`}
+        alt={title}
+        className="w-full h-full object-cover block lg:hidden"
       />
       <div className="content absolute top-0 left-0 w-full h-full text-center flex flex-col justify-end gap-8 px-5 pb-12 lg:px-40 lg:pb-24">
         <h2 className="font-fraunces font-extrabold capitalize text-2xl text-primary-dark-blue">
